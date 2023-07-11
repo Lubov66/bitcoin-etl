6c63c5e224f7d49faf881b1a76eb39abe25ae50a
AlanViast <AlanViast@gmail.com>
2023-07-11 19:02:17 +0800

    feat: rpc request retry

--- bitcoinetl/rpc/request.py
+++ bitcoinetl/rpc/request.py
@@ -1,6 +1,8 @@
 import hashlib
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
 
 _session_cache = {}
 
@@ -8,7 +10,13 @@ _session_cache = {}
 def _get_session(endpoint_uri):
     cache_key = hashlib.md5(endpoint_uri.encode('utf-8')).hexdigest()
     if cache_key not in _session_cache:
-        _session_cache[cache_key] = requests.Session()
+        endpoint_session = requests.Session()
+        retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[429, 500, 502, 503, 504])
+
+        endpoint_session.mount('http://', HTTPAdapter(max_retries=retries))
+        endpoint_session.mount('https://', HTTPAdapter(max_retries=retries))
+
+        _session_cache[cache_key] = endpoint_session
     return _session_cache[cache_key]
 
 
